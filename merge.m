bb6e68fc6dfcd92f2dd15c17bc26732cc44f3ce0
tepl <tepl@users.noreply.github.com>
2015-08-01 23:45:12 -0500

    Change images to transparent

--- merge.m
+++ merge.m
@@ -1,11 +1,22 @@
-nRows = 1;
-nCols = 15;
-imgCell = cell(nRows,nCols);
+for res = {'160','320'};
+    for item = {'cross','nought','splash'}
+        nRows = 1;
+        if(strcmp(item{1},'splash')==1)
+            nCols = 30;
+        else
+            nCols = 15;
+        end
+        imgCell = cell(nRows,nCols);
+        alpCell = cell(nRows,nCols);
 
-for iImage = 1:nRows*nCols
-    imageName = sprintf('320/nought/%04i.png',iImage);
-    imgCell{iImage} = imread(imageName);
+        for iImage = 1:nRows*nCols
+            imageName = sprintf('%s/%s/%04i.png',res{1},item{1},iImage);
+            [imgCell{iImage},map,alpCell{iImage}] = imread(imageName);
+        end
+
+        bigImage = cell2mat(imgCell);
+        alpImage = cell2mat(alpCell);
+        imageName = sprintf('final/%s%s.png',item{1},res{1});
+        imwrite(bigImage,imageName,'png','Alpha',alpImage);
+    end
 end
-
-bigImage = cell2mat(imgCell);
-imwrite(bigImage,'320/final/nought320.png')
